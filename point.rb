f0a3aa38bca2fecef524006d9246518b3e3c1f9c
kohey69 <kohey.chill.out@gmail.com>
2023-03-19 20:33:48 +0900

    9.6.1

--- point.rb
+++ point.rb
@@ -6,6 +6,18 @@ class Point
     @x, @y = x, y
   end
 
+  def inspect
+    "{#{x},#{y}}"
+  end
+
+  def +(other)
+    self.class.new(x + other.x, y + other.y)
+  end
+
+  def -(other)
+    self.class.new(x - other.x, y - other.y)
+  end
+
   def swap(other)
     tmp_x, tmp_y = @x, @y
     @x, @y = other.x, other.y
@@ -15,13 +27,19 @@ class Point
   end 
 end     
 
-p0 = Point.new
-p1 = Point.new(1.0, 2.0)
-p [p0.x, p0.y]
-p [p1.x, p1.y]
+# p0 = Point.new
+# p1 = Point.new(1.0, 2.0)
+# p [p0.x, p0.y]
+# p [p1.x, p1.y]
 
-p0.swap(p1)
-p [p0.x, p0.y]
-p [p1.x, p1.y]
+# p0.swap(p1)
+# p [p0.x, p0.y]
+# p [p1.x, p1.y]
 
-p0.x = 10.0
+# p0.x = 10.0
+
+point0 = Point.new(3,6)
+point1 = Point.new(1,8)
+
+p point0 + point1 #=> (4,14)
+p @x
