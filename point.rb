69a5621981bb2608aba355541c7a2f112b1058cf
kohey69 <kohey.chill.out@gmail.com>
2023-03-19 20:48:25 +0900

    9.6.2

--- point.rb
+++ point.rb
@@ -1,8 +1,8 @@
 class Point
   attr_accessor :x, :y
-  protected :x=, :y=
+  # protected :x=, :y=
 
-  def initialize(x=0.0, y=0.0)
+  def initialize(x=0, y=0)
     @x, @y = x, y
   end
 
@@ -18,13 +18,47 @@ class Point
     self.class.new(x - other.x, y - other.y)
   end
 
-  def swap(other)
-    tmp_x, tmp_y = @x, @y
-    @x, @y = other.x, other.y
-    other.x, other.y = tmp_x, tmp_y
+  # def swap(other)
+  #   tmp_x, tmp_y = @x, @y
+  #   @x, @y = other.x, other.y
+  #   other.x, other.y = tmp_x, tmp_y
 
-    return self
-  end 
+  #   return self
+  # end 
+
+  def +@
+    dup
+  end
+  
+  def -@
+    self.class.new(-x, -y)
+  end
+
+  def ~@
+    self.class.new(-y, x)
+  end
+
+  def [](index)
+    case index
+    when 0 
+      x
+    when 1
+      y
+    else
+      raise ArgumentError, "out of range `#{index}'"
+    end
+  end
+
+  def []=(index, val)
+    case index
+    when 0
+      self.x = val
+    when 1
+      self.y = val
+    else
+      raise ArgumentError, "out of range `#{index}'"
+    end
+  end
 end     
 
 # p0 = Point.new
@@ -38,8 +72,19 @@ end
 
 # p0.x = 10.0
 
-point0 = Point.new(3,6)
-point1 = Point.new(1,8)
+# point0 = Point.new(3,6)
+# point1 = Point.new(1,8)
 
-p point0 + point1 #=> (4,14)
-p @x
+# p point0 + point1 #=> (4,14)
+# p @x
+
+# point = Point.new(3, 6)
+# p +point
+# p -point
+# p ~point
+
+point = Point.new(3,6)
+p point[0]
+p point[1] = 2
+p point[1]
+p point[2]
